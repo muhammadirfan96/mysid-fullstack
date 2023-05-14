f9a0c59e958c78a8e1ec02fe756f92b71c6dbdcf
muhammadirfan96 <muhammadirfanirfani808@gmail.com>
2023-05-14 13:01:37 +0800

    perbaiki lebar tampilan admin web

--- app/Views/menubar/admin_web.php
+++ app/Views/menubar/admin_web.php
@@ -66,7 +66,7 @@ $menuadminweb = [
     <div class="m-2 flex flex-wrap justify-evenly gap-1">
 
         <?php foreach ($menuadminweb as $key => $value) : ?>
-            <div class="relative w-full md:w-40 xl:w-80 h-32 border-2 border-cyan-900 rounded-md bg-cyan-50 mb-2" style="background-image: <?= $value['bg_src'] ?>; background-size: cover;">
+            <div class="relative w-[48%] md:w-[24%] xl:w-[19%] h-32 border-2 border-cyan-900 rounded-md bg-cyan-50 mb-2" style="background-image: <?= $value['bg_src'] ?>; background-size: cover;">
                 <a href="<?= base_url($value['link']); ?>" class="absolute bg-cyan-900 rounded-b-sm w-full bottom-0 text-white text-center py-2"><?= $key ?></a>
             </div>
         <?php endforeach ?>
